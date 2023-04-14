03ec9aad530e804d4d73d27fe1fa2e24dea391e4
liamreeves <liamreeves42@gmail.com>
2023-04-14 17:36:14 +0200

    Added test suite for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("renders the 'New Word' button", () => {
+    render(<App />);
+    const newWordButton = screen.getByRole("button", { name: /new word/i });
+    expect(newWordButton).toBeInTheDocument();
+  });
+
+  test("renders the tooltip with the correct text", () => {
+    render(<App />);
+    const tooltipTrigger = screen.getByText("?");
+    fireEvent.click(tooltipTrigger);
+    const tooltipText = screen.getByText(
+      /the dictionary of obscure sorrows is a website/i
+    );
+    expect(tooltipText).toBeInTheDocument();
+  });
+
+  test("renders the WordDropdown component", () => {
+    render(<App />);
+    const wordDropdown = screen.getByRole("combobox");
+    expect(wordDropdown).toBeInTheDocument();
+  });
 });
